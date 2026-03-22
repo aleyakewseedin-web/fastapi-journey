e8a43081c7d9833202be44b9aa97a9e7a3caa566
Your Name <you@example.com>
2026-03-22 13:22:54 +0200

    mini-project-1: add Jinja2 templating

--- mini-project-1/enrollment.py
+++ mini-project-1/enrollment.py
@@ -1,25 +1,30 @@
 import asyncio
-from fastapi import APIRouter, HTTPException
-from models import Enrollment
-from models  import Student
+from fastapi import APIRouter, HTTPException,Request
+from models.enrollment import Enrollment
+from models.student  import Student
+from models.enrollment import Semester
 from typing import List
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 
 enrollment=APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 students: List[Student] = [
     Student(
         id=1,
         name="Alice",
-        major="Computer Science",
+        department="Computer Science",
         enrollments=[
-            Enrollment(course_name="Databases", semester= Semester.fall, grade=88,instructor="Dr. Smith"  ), # type: ignore
-            Enrollment(course_name="Algorithms", semester = Semester.spring, grade=92) # type: ignore
+            Enrollment(course_name="Databases", semester= Semester.fall, grade=88,instructor="Dr. Smith"  ),
+            Enrollment(course_name="Algorithms", semester = Semester.spring, grade=92) 
         ]
     ),
     Student(
         id=2,
         name="Bob",
-        major="Engineering",
+        department="Engineering",
         enrollments=[]
     )]
 @enrollment.post("/students/")
@@ -29,7 +34,7 @@ async def create_student(student: Student):
     student_id= max([s.id for s in students]) + 1 if students else 1
     new_student= Student(id=student_id, 
                          name=student.name, 
-                         major=student.major, 
+                         department=student.department, 
                          enrollments=student.enrollments)
     await asyncio.sleep(1)
     students.append(new_student) 
@@ -56,7 +61,7 @@ async def update_student(student_id: int, updated_student: Student):
     for student in students:
         if student.id==student_id:
             student.name= updated_student.name
-            student.major= updated_student.major
+            student.department= updated_student.department
             student.enrollments= updated_student.enrollments
             return student
     raise HTTPException(status_code=404, detail=f"Student with this ID {student_id} is not found")
@@ -68,3 +73,20 @@ async def delete_student(student_id: int):
             students.remove(student)
             return {"message": "Student deleted successfully"}
     raise HTTPException(status_code=404, detail=f"Student with this ID {student_id} is not found")
+
+@enrollment.get("/home", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "items": students
+    })
+
+@enrollment.get("/course_enrollment/{id}", response_class=HTMLResponse)
+async def get_item_page(request: Request, id: int):
+    for student in students:
+        if student.id == id:
+            return templates.TemplateResponse("enrollment.html", {
+                "request": request,
+                "enrollment": student
+            })
+    raise HTTPException(status_code=404, detail="student is not found")
--- mini-project-1/main.py
+++ mini-project-1/main.py
@@ -1,11 +1,11 @@
-import asyncio
-from fastapi import FastAPI, HTTPException
-from typing import List
-from models import Student
-from models.enrollment import Semester
-from models.enrollment import Enrollment
 
-
+from fastapi import FastAPI
+from enrollment import enrollment
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="templates") 
 app= FastAPI()  
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Welcome to the enrollment API"}
 
-
+app.include_router(enrollment)
