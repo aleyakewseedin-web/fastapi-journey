01add472e6ff0c00f8768b112d2b4fef47aae7e2
Your Name <you@example.com>
2026-03-29 11:43:40 +0300

    mini-project-2: define Event and User Beanie documents

---
+++ mini-project-2/models/users.py
@@ -0,0 +1,22 @@
+from typing import Optional, List
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
+from models.events import Event
+
+class User(Document):
+ email: EmailStr
+ password: str
+ events: Optional[List[Link[Event]]]
+ class Settings:
+     name = "users"
+ class Config:
+   schema_extra = {
+   "example": {
+   "email": "fastapi@packt.com",
+   "password": "strong!!!",
+   "events": [],
+ }
+ }
+class UserSignIn(BaseModel):
+ email: EmailStr
+ password: str
